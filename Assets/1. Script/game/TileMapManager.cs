d3bdf18fbff838b95a21b73d5d94a206aa243f35
leeeungu <74060680+leeeungu@users.noreply.github.com>
2024-02-14 17:46:34 +0900

    update

--- Assets/1. Script/game/TileMapManager.cs
+++ Assets/1. Script/game/TileMapManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class TileMapManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (!collision.CompareTag("Area"))
+            return;
+
+        Vector3 playerPos = GameManager._instance._characterManager.transform.position;
+        Vector3 myPos = transform.position;
+
+        float dirX = playerPos.x - myPos.x;
+        float dirY = playerPos.y - myPos.y;
+
+        float diffX = Mathf.Abs(dirX);
+        float diffY = Mathf.Abs(dirY);
+
+        dirX = dirX > 0 ? 1 : -1;
+        dirY = dirY > 0 ? 1 : -1;
+
+        switch(transform.tag)
+        {
+            case "Ground":
+                if(Mathf.Abs(diffX - diffY)<= 0.1f)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if(diffX > diffY)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else if(diffX < diffY)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
